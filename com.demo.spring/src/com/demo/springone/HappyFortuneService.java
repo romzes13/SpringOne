c86237b7c2755c27ecbc50c13dd96a66138b51ba
Roman Developer <romzes13@gmail.com>
2016-12-25 15:08:33 -0500

    Displays a random string from an array.
    Signed-off-by: Roman Developer <romzes13@gmail.com>

--- com.demo.spring/src/com/demo/springone/HappyFortuneService.java
+++ com.demo.spring/src/com/demo/springone/HappyFortuneService.java
@@ -1,11 +1,22 @@
 package com.demo.springone;
 
+import java.util.Random;
+
 public class HappyFortuneService implements FortuneService{
 
 	@Override
 	public String getFortune() {
 
-		return "Today is your lucky day!";
+		// Declaring a new String array
+		String[] myList = {"1st Lucky day!","2nd Lucky day!","3rd Lucky day!"};
+		
+		// Generating a random number
+		Random rand = new Random(); 
+		int x = rand.nextInt(myList.length);
+		
+		System.out.println("Random number is : "+x);
+		
+		return myList[x];
 	}
 
 }
